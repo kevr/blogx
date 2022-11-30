e65dda025ebbba77962f8c3828c9603d0074ccd4
Kevin Morris <kevr@0cost.org>
2022-11-30 07:29:04 -0800

    feat(django): add Post model
    Fields:
    - id: Primary key
    - author: Foreign key to django user
    - content: Text
    - created: DateTime
    - edited: DateTime, nullable
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.db.models import DateTimeField, ForeignKey, IntegerField, TextField
 
-# Create your models here.
+
+class Post(models.Model):
+    id = IntegerField(primary_key=True)
+
+    author = ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    content = TextField()
+    created = DateTimeField()
+    edited = DateTimeField(blank=True, null=True)
