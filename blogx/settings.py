ad2236e78a7e09ddb765389f0d57830ff9056e0e
Kevin Morris <kevr@0cost.org>
2022-11-30 06:26:24 -0800

    feat: support SECRET_KEY envvar override
    This should be set in production.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- blogx/settings.py
+++ blogx/settings.py
@@ -9,18 +9,17 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0=vtb2nsvjc*u&cfgz)o!-f+1^)f6mjwy%#f^tw9a8"
+# SECURITY WARNING: In production, the SECRET_KEY envvar
+# should be set in django's environment.
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-0=vtb2nsvjc*u&cfgz)o!-f+1^)f6mjwy%#f^tw9a8"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
