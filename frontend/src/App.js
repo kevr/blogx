a4e6da7de8077c07a462b9d076e6535211450d0f
Kevin Morris <kevr@0cost.org>
2023-01-04 08:02:53 -0800

    change: relocate main app render into <Entry>
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,12 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
-import logo from "./logo.svg";
+import Entry from "./Entry";
 import "./App.css";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Entry />
     </Provider>
   );
 }
---
+++ frontend/src/Entry.js
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import logo from "./logo.svg";
+
+const Entry = () => {
+  const session = useSelector((state) => state.session);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Entry;
