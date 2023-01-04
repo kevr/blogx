615faeb7504be3c4f352484c639417cd1269cf97
Kevin Morris <kevr@0cost.org>
2023-01-04 09:22:44 -0800

    change(frontend): lowercase class names
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- frontend/src/Entry.js
+++ frontend/src/Entry.js
@@ -3,22 +3,21 @@ import logo from "./logo.svg";
 
 const Entry = () => {
   const session = useSelector((state) => state.session);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={logo} className="app-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="app-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 };
