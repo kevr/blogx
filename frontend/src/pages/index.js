f109bc880087baca4fa75d13e28c50bdb482a31f
Kevin Morris <kevr@0cost.org>
2023-01-05 13:02:45 -0800

    fix(frontend): give names to default exports
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,9 @@
 import Home from "./Home";
 import Login from "./Login";
 
+// Optional exports
 export { Home, Login };
 
-export default {
-  Home,
-  Login,
-};
+// Default export
+const exports = { Home, Login };
+export default exports;
