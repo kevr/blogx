46d744deace94c15ad0c5d1c51ef85322c62aab7
Kevin Morris <kevr@0cost.org>
2023-01-14 06:48:38 -0800

    change(django): increase JWT lifetimes
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- blogx/settings.py
+++ blogx/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +138,8 @@ if not DEBUG:
 
 CORS_ALLOWED_ORIGINS = [f"{scheme}://{host}" for host in HOSTS]
 print(f"Allowed origins: {CORS_ALLOWED_ORIGINS}")
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
