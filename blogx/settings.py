7be2623072516ed2a9c65f13ee3d50275e37fb44
Kevin Morris <kevr@0cost.org>
2023-01-14 06:46:47 -0800

    fix(django): replace broken django-cors-headers with own middleware
    django-cors-headers, for some reason, was not supplying headers
    that it should've been. This implementation was directly derived
    off of https://stackoverflow.com/questions/28046422/django-cors-headers-not-work
    with customization to actually include approved origins instead of
    '*'.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- blogx/settings.py
+++ blogx/settings.py
@@ -38,14 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
     "rest_framework",
     "api",
 ]
 
 # Middleware
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+    "api.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -136,6 +135,5 @@ scheme = "http"
 if not DEBUG:
     scheme = "https"  # pragma: no cover
 
-CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [f"{scheme}://{host}" for host in HOSTS]
 print(f"Allowed origins: {CORS_ALLOWED_ORIGINS}")
